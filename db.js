4caf85882eb5f261bd667315b4b555295694488d
acronisruu-coder <acronisruu@gmail.com>
2026-01-12 13:36:42 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('./warehouse.db');
+const db = new sqlite3.Database("./warehouse.db");
 
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS sensor_data (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      device_id TEXT,
+      warehouse TEXT,
       temperature REAL,
       humidity REAL,
-      battery REAL,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     )
   `);
