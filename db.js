816f8b9da796ce8a9ad4550769d4cbea14de4f72
acronisruu-coder <acronisruu@gmail.com>
2026-01-12 13:41:21 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./warehouse.db");
+const db = new sqlite3.Database("./warehouse_v2.db");
 
 db.serialize(() => {
   db.run(`
